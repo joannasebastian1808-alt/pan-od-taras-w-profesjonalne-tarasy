feddfef5964302b8f3330a4f89cd6442289160d9
joannasebastian1808-alt <joannasebastian1808@gmail.com>
2026-06-04 21:53:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/pan-od-taras-w-profesjonalne-tarasy/",
+    base: "/pan-od-tarasow/",
   },
   tanstackStart: {
     prerender: {
